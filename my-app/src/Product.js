a86243776d91c0b5ef26f5d89cd4948577ff308a
Mighty-Geek <aamishajangra@gmail.com>
2022-10-18 02:12:42 +0530

    initial commit

---
+++ my-app/src/Product.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+    handleUpVote() {
+        this.props.onVote(this.props.id);
+    }
+    render() {
+        return (
+            <div className='item'>
+                <div>
+                    <img className='p-image' src={this.props.productImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a onClick={this.handleUpVote} className='caret-up'>
+                            🔺
+                        </a>
+                        {this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by: </span>
+                        <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Product;
